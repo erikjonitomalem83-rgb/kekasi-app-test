51d0ed9950d59b2c00a19e4d63a7911ddf098783
kepegkanimsiantar <kepeg.kanim.siantar@gmail.com>
2026-01-04 10:11:36 +0700

    Menu Riwayat Nomor menjadi paling atas

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -22,17 +22,17 @@ export default function Sidebar({
 
   const menuItems = [
     {
-      label: "Rekap",
-      icon: "M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z",
-      color: "bg-green-600",
-      onClick: onShowRekap,
-      show: true,
-    },
-    {
       label: "Riwayat Nomor",
       icon: "M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z",
       color: "bg-blue-600",
       onClick: onShowHistory,
+      show: true,
+    },
+    {
+      label: "Rekap",
+      icon: "M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z",
+      color: "bg-green-600",
+      onClick: onShowRekap,
       show: true,
     },
     {
